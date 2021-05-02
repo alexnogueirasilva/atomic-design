806e43837b8751bcde3d46b2520d89c54b60b221
Alex Nogueira <alex@devaction.com.br>
2021-05-02 11:54:41 -0300

    criando atomo Logo

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="component">
+    <Logo />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Logo } from '@/components/atomos'
+
+export default defineComponent({
+  components: { Logo }
+})
+</script>
+
+<style lang="scss" scoped>
+.component {
+}
+</style>
